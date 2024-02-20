043af841278ffd42697a9016d70cfe4e7c9962a8
aritunaeru <kryslerbal@outlook.com>
2024-02-20 19:49:11 +0800

    chore: initial commit

--- src/script.js
+++ src/script.js
@@ -1,14 +1,37 @@
-const attendeeIds = readDatabase().attendeeIds || {};
-let previousEntries = readDatabase().previousEntries || [];
-console.log(previousEntries);
-const snackbar = document.getElementById('snackbar');
 const currentTime = new Date().getTime();
+let previousEntries = [];
 
-function showError(errMsg) {
-	snackbar.className = 'show';
-	snackbar.textContent = String(errMsg);
-	setTimeout(function() {
-		snackbar.className = snackbar.className.replace('show', '');
+//  fetch data from data.json
+async function fetchData() {
+	try {
+		const response = await fetch('data.json');
+		if (!response.ok) {
+			throw new Error('Failed to fetch data');
+		}
+		const data = await response.json();
+
+		previousEntries = data.previousEntries || [];
+		displayPreviousEntries();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+fetchData();
+
+let timeoutId; // Variable to store the timeout ID
+
+function showToast(str) {
+	const snackbar = document.getElementById('snackbar');
+	snackbar.textContent = String(str);
+	snackbar.style.visibility = 'visible';
+
+	// Clear any existing timeout
+	clearTimeout(timeoutId);
+
+	// Set a new timeout
+	timeoutId = setTimeout(function() {
+		snackbar.style.visibility = 'hidden';
 	}, 3000);
 }
 
@@ -17,70 +40,148 @@ function generateUniqueId() {
 	return `${Date.now().toString(36)}${randomNo}`;
 }
 
-function logAttendance() {
-	const inputName = String(document.getElementById('attendeeName').value);
+async function logAttendance() {
+	displayPreviousEntries();
+
+	let inputName = String(document.getElementById('attendeeName').value);
+	// ipacapital tanan letters!
+	inputName = inputName?.toUpperCase();
+	const cleanedInput = cleanSearchInput(inputName);
+	inputName = cleanedInput;
 	if (inputName === '') {
-		showError('Invalid input. The name is blank, please input a name.');
+		showToast('Invalid input. The name is either blank or not a name. Please input a name.');
 		return;
 	}
 
-	console.log(inputName.length);
 	if (inputName.length > 30) {
-		showError('Invalid input. You have reached the maximum character limit.');
+		showToast('Invalid input. You have reached the maximum character limit.');
 		return;
 	}
-	// kung pariha ang pangalan ang gi input sa user kumpara sa na save sa localstorage
-	if (previousEntries?.some(entry => entry.inputName === inputName)) {
-		showError('Invalid input. The name already exists.');
+
+	if (previousEntries.some(entry => entry.inputName === inputName)) {
+		showToast(`Invalid input. ${inputName} already exists.`);
 		return;
 	}
+
+	showToast(`Successfully added ${inputName}`);
+
+
 	const id = generateUniqueId();
 	const time = formatDate(new Date());
-	// Display the ID
 	document.getElementById('attendeeId').textContent = `Your ID: ${id}`;
 
-	// Store the ID
-	attendeeIds[inputName] = id;
-	attendeeIds[time] = time;
-	localStorage.setItem('attendeeIds', JSON.stringify(attendeeIds));
-
-	// Add to previous entries
 	previousEntries.push({
 		inputName,
 		id,
 		time,
 	});
-	localStorage.setItem('previousEntries', JSON.stringify(previousEntries));
 
-	// Display previous entries
+	await writeData(previousEntries);
+
 	displayPreviousEntries();
 }
+
+const ENTRIES_PER_PAGE = 5;
+let currentPage = 1;
 
 function displayPreviousEntries() {
 	const entriesDiv = document.getElementById('previousEntries');
 
-	entriesDiv.innerHTML = '<h2>Previous Entries:</h2>';
+	const startIndex = (currentPage - 1) * ENTRIES_PER_PAGE;
+	const endIndex = startIndex + ENTRIES_PER_PAGE;
 
-	// Loop through previousEntries array and append each entry to entriesDiv
+	entriesDiv.innerHTML = '<h2>Previous Entries: </h2>';
 
-	previousEntries.forEach((entry, index) => {
-		entriesDiv.innerHTML += `<p><span class="index">${index + 1}.</span> <span class="entry">Name: ${entry.inputName}<br>Entry ID: ${entry.id}<br>Entry Time: ${entry.time}</span></p>`;
+	previousEntries.slice(startIndex, endIndex).forEach((entry, index) => {
+		entriesDiv.innerHTML += `<p><span class="index">${startIndex + index + 1}.</span> <span class="entry">Name: ${entry.inputName}<br>Entry ID: ${entry.id}<br>Entry Time: ${entry.time}</span></p>`;
 	});
+
+	if (previousEntries.length > ENTRIES_PER_PAGE) {
+		renderPaginationControls(entriesDiv);
+	}
 }
 
-// Function to clear old entries
-function clearOldData() {
-	const twentyTwoHours = 22 * 60 * 60 * 1000; // 22 hours in milliseconds
+function renderPaginationControls(entriesDiv) {
+	const totalPages = Math.ceil(previousEntries.length / ENTRIES_PER_PAGE);
 
-	previousEntries = previousEntries.filter(entry => {
+	const paginationDiv = document.createElement('div');
+	paginationDiv.classList.add('pagination');
+	paginationDiv.style.display = 'flex';
+
+	const pageInfo = document.createElement('span');
+	pageInfo.textContent = `Page ${currentPage} of ${totalPages}`;
+	paginationDiv.appendChild(pageInfo);
+
+	const buttonsContainer = document.createElement('div');
+	buttonsContainer.style.marginLeft = 'auto';
+	paginationDiv.appendChild(buttonsContainer);
+
+	// Prev button
+	const prevButton = document.createElement('button');
+	prevButton.textContent = 'Prev';
+	prevButton.style.marginRight = '5px';
+	prevButton.addEventListener('click', () => {
+		if (currentPage > 1) {
+			currentPage--;
+			displayPreviousEntries();
+		} else {
+			showToast('You are already at the first page.');
+			return;
+		}
+	});
+	buttonsContainer.appendChild(prevButton);
+
+	const nextButton = document.createElement('button');
+	nextButton.textContent = 'Next';
+	nextButton.addEventListener('click', () => {
+		if (currentPage < totalPages) {
+			currentPage++;
+			displayPreviousEntries();
+		} else {
+			showToast('You are already at the last page.');
+			return;
+		}
+	});
+	buttonsContainer.appendChild(nextButton);
+
+	entriesDiv.appendChild(paginationDiv);
+}
+
+async function writeData(data) {
+	console.log('Writing data...');
+	try {
+		await fetch('data.json', {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ previousEntries: data }, null, 2),
+		});
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function clearOldData() {
+	const twentyTwoHours = 22 * 60 * 60 * 1000;
+
+	const filteredEntries = previousEntries.filter(entry => {
 		const entryTime = new Date(entry.time).getTime();
+		console.log('Entry time:', entryTime);
 		return currentTime - entryTime <= twentyTwoHours;
 	});
 
-	localStorage.setItem('previousEntries', JSON.stringify(previousEntries));
+	if (filteredEntries.length === previousEntries.length) {
+		showToast('No entries older than 22 hours.');
+		return;
+	}
+
+	showToast('Cleared data older than 22 hours.');
+
+	previousEntries = filteredEntries;
+	await writeData(previousEntries);
+	displayPreviousEntries();
 }
-// Call the function only once
-displayPreviousEntries();
 
 
 function formatDate(date) {
@@ -94,3 +195,51 @@ function formatDate(date) {
 	};
 	return date.toLocaleDateString('en-US', options);
 }
+
+function searchEntries() {
+	const searchInput = document.getElementById('searchInput').value.trim().toLowerCase();
+
+	if (searchInput === '') {
+		showToast('Search input is empty.');
+		displayPreviousEntries();
+		return;
+	}
+
+	if (searchInput.length > 30) {
+		showToast('Invalid search. You have reached the maximum character limit.');
+		return;
+	}
+
+	const searchResults = previousEntries.filter(entry => {
+		const formattedTime = formatDate(new Date(entry.time));
+		return entry.inputName.toLowerCase().includes(searchInput) || entry.id.includes(searchInput) || formattedTime.includes(searchInput);
+	});
+
+	const entriesDiv = document.getElementById('previousEntries');
+	entriesDiv.innerHTML = '<h2>Search Results:</h2>';
+
+	if (searchResults.length === 0) {
+		entriesDiv.innerHTML += '<p>No matching entries found.</p>';
+		showToast('No matching entries found.');
+		return;
+	}
+	showToast(`Found ${searchResults.length} entr${searchResults.length > 1 ? 'ies' : 'y'}.`);
+
+	searchResults.forEach((entry, index) => {
+		entriesDiv.innerHTML += `<p><span class="index">${index + 1}.</span> <span class="entry">Name: ${entry.inputName}<br>Entry ID: ${entry.id}<br>Entry Time: ${entry.time}</span></p>`;
+	});
+}
+
+const cleanSearchInput = (input) => {
+	const desired = input.replace(/[^\w\s.]/gi, ''); // Exclude dots from replacement
+	return desired;
+};
+
+document.addEventListener('keydown', async (event) => {
+	if (event.key === 'Enter' && event.target.id === 'attendeeName') {
+		await logAttendance();
+	}
+	if (event.key === 'Enter' && event.target.id === 'searchInput') {
+		searchEntries()();
+	}
+});
--- src/server.js
+++ src/server.js
@@ -3,9 +3,10 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
+	console.log(req.url);
 	let filePath = '.' + req.url;
 	if (filePath === './') {
-		filePath = './index.html';
+		filePath = './src/index.html';
 	}
 
 	const extname = String(path.extname(filePath)).toLowerCase();
@@ -19,20 +20,44 @@ const server = http.createServer((req, res) => {
 
 	contentType = mimeTypes[extname] || 'application/octet-stream';
 
-	fs.readFile(filePath, (err, content) => {
-		if (err) {
-			if (err.code === 'ENOENT') {
-				res.writeHead(404);
-				res.end('404 Not Found');
-			} else {
-				res.writeHead(500);
-				res.end('500 Internal Server Error: ' + err.code);
+	if (req.method === 'PUT' && req.url === '/data.json') {
+		let body = '';
+		req.on('data', (chunk) => {
+			body += chunk.toString();
+		});
+		req.on('end', () => {
+			try {
+				const data = JSON.parse(body);
+				fs.writeFile('./data.json', JSON.stringify(data, null, 2), (err) => {
+					if (err) {
+						res.writeHead(500);
+						res.end('500 Internal Server Error: ' + err.code);
+					} else {
+						res.writeHead(200, { 'Content-Type': 'application/json' });
+						res.end(JSON.stringify({ message: 'Data updated successfully' }, null, 2));
+					}
+				});
+			} catch (error) {
+				res.writeHead(400);
+				res.end('400 Bad Request: ' + error.message);
 			}
-		} else {
-			res.writeHead(200, { 'Content-Type': contentType });
-			res.end(content, 'utf-8');
-		}
-	});
+		});
+	} else {
+		fs.readFile(filePath, (err, content) => {
+			if (err) {
+				if (err.code === 'ENOENT') {
+					res.writeHead(404);
+					res.end('404 Not Found');
+				} else {
+					res.writeHead(500);
+					res.end('500 Internal Server Error: ' + err.code);
+				}
+			} else {
+				res.writeHead(200, { 'Content-Type': contentType });
+				res.end(content, 'utf-8');
+			}
+		});
+	}
 });
 
 const port = process.env.PORT || 3000;
